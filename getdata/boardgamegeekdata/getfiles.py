9596819ea949e698037e15069280b91a9cd15424
Courtney Epstein <courtney_epstein@yahoo.com>
2014-08-04 22:49:29 -0400

    - Added comments to getfiles.py - parse_game_data.py is making progress; able to create Preferences MySQL table in boardgamegeek database, next steps: Games and Basics tables - Found list of game categories and mechanics on boardgamegeek.com

--- getdata/boardgamegeekdata/getfiles.py
+++ getdata/boardgamegeekdata/getfiles.py
@@ -4,6 +4,7 @@ import os
 
 
 def get_boardgamegeek_url(gameid,page):
+    '''Returns the website for the boardgamegeek.com API that has the info on board games, user ratings, and statistics'''
     url='http://boardgamegeek.com/xmlapi2/thing?type=boardgame&ratingcomments=1&stats=1&id={0}&page={1}'.format(gameid,page)
     return urllib2.urlopen(url).read()
 
@@ -11,33 +12,40 @@ def get_boardgamegeek_url(gameid,page):
 def main():
     # gameid=40834
     
-    for gameid in range(87119,150000):
+    for gameid in range(1,180000):
         skip=False
         page=1
-        
+    
         result=get_boardgamegeek_url(gameid,page)
         strainer=SoupStrainer("comments")
+        '''Only scrape pages that had >0 user comments/ratings'''
         try:
             n_comments=int(BeautifulSoup(result,"xml",parse_only=strainer).comments['totalitems'])
         except:
             skip=True
             print "skipping {0}".format(gameid)
 
+        '''The boardgamegeek API only lets you download 100 user comments/ratings at one time.
+        This figures out how many pages of comments exist'''
         if not skip:
             n_pages=n_comments//100
             if n_comments % 100 != 0:
                 n_pages+=1
             
-            
+            '''Put the information for each game in its own folder called game<id#>
+            If that folder does not already exist, make the directory'''
             outfolder='/Users/athena/Insight/Boardgame_Project/getdata/boardgamegeekdata/game{0}'.format(gameid)
             if not os.path.exists(outfolder):
                 os.mkdir(outfolder)    
             
+
             # for page in [1]:
             for page in range(1,n_pages+1):
                 print '{0} of {1} pages of comments'.format(page,n_pages)
                 if(page != 1):
+                    '''We've already gotten the first page, but we need to get the following pages, if any'''
                     result=get_boardgamegeek_url(gameid,page)
+                '''Write each page of comments to its own .xml file, called game<id#>_page<page#>'''
                 outfile='game{0}_page{1}.xml'.format(gameid,page)
                 # print outfile
                 f=open(outfolder+'/'+outfile,'w')
--- getdata/parse_game_data.py
+++ getdata/parse_game_data.py
@@ -1,89 +1,162 @@
 from bs4 import BeautifulSoup
 import os 
 import re
-import pandas as pd
+# import pandas as pd
 import numpy as np
+import MySQLdb as mdb
 
-def function(name):
+
+def paginator(name):
     return int(re.findall('page(.+)\.',name)[0])
 
 def get_names(gameid):
-    folder='boardgamegeekdata/game{0}/'.format(gameid)
-    filenames=sorted(os.listdir(folder),key=function)
+    folder=os.path.abspath('boardgamegeekdata/game{0}/'.format(gameid))
+    if os.path.exists(folder):
+        filenames=sorted(os.listdir(folder),key=paginator)
+    else:
+        filenames=None
     return folder,filenames
 
-def plothistogram(xmin,xmax,values,axis,color='Red',nbin=40,normalize=False):
-    n,binEdges=np.histogram(values,bins=nbin,range=(xmin,xmax))
-    bincenters = 0.5*(binEdges[1:]+binEdges[:-1])
-    width=binEdges[1:]-binEdges[:-1]
-    if(normalize==True):
-        norm=[float(n[i])/float(sum(n)) for i in range(len(n))]
-        axis.bar(bincenters,norm,width=width,facecolor=color)
-    else:
-        axis.bar(bincenters,n,width=width,facecolor=color)
-    return bincenters,n
+def get_viable_game_ids():
+    '''Searches the folder containing all of the data scraped from boardgamegeek.com
+    '''
+    abspath=os.path.abspath('boardgamegeekdata')
+    contents=os.listdir(abspath)
+    all_gameid=[int(name[4:]) for name in contents if(name[0:4]=='game')]
+    return all_gameid
 
-# gameid=40834
-# 36218
-# gameid=9209
-gameid=13
-folder,filenames=get_names(gameid)
-get_names(gameid)
-
-for i,filename in enumerate(filenames):
-# for i,filename in enumerate([filenames[0],filenames[1]]):
-    soup=BeautifulSoup(open(folder+filename), "xml")
-    comments=soup.find_all('comment')
-    if(i==0):
-        boardgame_name=soup.find("name").attrs['value']
-        boardgame_id=soup.find(type="boardgame")['id']
-        boardgame_family_name=soup.find(type="boardgamefamily")['value']
-        boardgame_family_id=soup.find(type="boardgamefamily")['id']
-        boardgame_year=int(soup.yearpublished['value'])
-        boardgame_min_player=int(soup.minplayers['value'])
-        boardgame_max_player=int(soup.maxplayers['value'])
-        boardgame_play_time=int(soup.playingtime['value'])
-        boardgame_image=soup.image.text
-        boardgame_mechanic=[ mechanic.attrs['value'] for mechanic in soup.find_all('link',type='boardgamemechanic')]
-        boardgame_weight=float(soup.averageweight['value'])
-        user_name=[comment.attrs['username'] for comment in comments]
-        user_rating=[float(comment.attrs['rating']) for comment in comments]
-    else:
-        [user_name.append(comment.attrs['username']) for comment in comments]
-        [user_rating.append(float(comment.attrs['rating'])) for comment in comments]
-        print len(user_rating)
-
-user=pd.Series(data=user_rating,index=user_name)
-# count,division = np.histogram(user)
-
-# print boardgame_name
-# print boardgame_id
-# print boardgame_family_name
-# print boardgame_family_id
-# print boardgame_min_player,boardgame_max_player
-# print boardgame_play_time
-# print boardgame_year
-# print boardgame_image
-# print boardgame_mechanic
-# print boardgame_weight
-# print users
+all_gameid=get_viable_game_ids()
 
 
-fig=plt.figure()
-xmin=0.5
-xmax=10.5
-ymin=0
-ymax=16000
+# con = mdb.connect('localhost', 'root', 'mandrake', 'boardgamegeek')
 
-ax = fig.add_subplot(111)
-plt.xlabel('User Rating',size=16)
-plt.ylabel('N',size=16)
-plt.xticks(np.arange(1, 11, 1))
-ax.set_xlim(xmin,xmax)
-ax.set_ylim(ymin,ymax)
-ax.annotate('{0}'.format(boardgame_name), xy=(0.05, 0.85), xycoords="axes fraction",size=20)
-user.hist(bins=range(12),ax=ax,align='left',grid=False)
 
-fig.savefig('game{0}.eps'.format(gameid))
+# # gameid=40834
+# # 36218
+# # gameid=9209
+# # gameid=34635
+# # for gameid in range(1,21):
+# for gameid in range(1,5):
+#     print gameid
+#     folder,filenames=get_names(gameid)
+#     print folder, filenames
+#     # get_names(gameid)
+    
+#     bg={}
+    
+#     with con:
+#         cur = con.cursor()
+#         # if(gameid == 1):
+#             ## MAKE TABLE THAT CONTAINS USER PREFERENCES
+#             # cur.execute('DROP TABLE IF EXISTS Preferences')
+#             # cur.execute("CREATE TABLE `Preferences`(\
+#             # `USER_NAME` CHAR(20),\
+#             # `BOARDGAME_ID` INT(6),\
+#             # `BOARDGAME_NAME` CHAR(100),\
+#             # `USER_RATING` DECIMAL(5,2)\
+#             # ) DEFAULT CHARSET=utf8;")
+#             ## MAKE TABLE THAT CONTAINS BOARDGAME ATTRIBUTES
+#             # cur.execute('DROP TABLE IF EXISTS Games')
+#             # cur.execute("CREATE TABLE `Games`(\
+#             # `BOARDGAME_ID` INT(6),\
+#             # `BOARDGAME_NAME` CHAR(100),\
+#             # `USER_RATING` DECIMAL(5,2),\
+#             # PRIMARY KEY (`BOARDGAME_ID`),
+#             # ) DEFAULT CHARSET=utf8;")
+#             ## MAKE TABLE THAT CONTAINS BOARDGAME INFORMATION FOR DISPLAY ON WEBSITE
+#             # cur.execute('DROP TABLE IF EXISTS Basics')
+#             # cur.execute("CREATE TABLE `Basics`(\
+#             # `BOARDGAME_ID` INT(6),\
+#             # `BOARDGAME_NAME` CHAR(100),\
+#             # # `USER_RATING` DECIMAL(5,2)\
+#             # ) DEFAULT CHARSET=utf8;")
+    
+#         for i,filename in enumerate(filenames):
+#             # for i,filename in enumerate([filenames[0],filenames[1]]):
+#         # for i,filename in enumerate([filenames[0]]):
+#             soup=BeautifulSoup(open(folder+filename), "xml")
+#             comments=soup.find_all('comment')
+#             # categories=soup.find_all(type="boardgamecategory")
+#             #     # mechanics=soup.find_all(type="boardgamemechanic")
+#             #     # expansions=soup.find_all(type="boardgameexpansion")
+#             if(i==0):
+#                 bg['BOARDGAME_NAME']=soup.find("name",type="primary").attrs['value']
+#                 bg['BOARDGAME_ID']=soup.find(type="boardgame")['id']
+#         #         # bg['MIN_PLAYER_MANUFACTURER']=int(soup.minplayers['value'])
+#         #         # bg['MAX_PLAYER_MANUFACTURER']=int(soup.maxplayers['value'])
+#         #     #     # 30 looks like the maximum number of players 
+#         #     #     num_players={}
+#         #         # for nplayers in range(boardgame_min_player,boardgame_max_player+1):
+#         #     #         poll_result=soup.find("results",numplayers=nplayers)
+#         #     #         votes=[int(child['numvotes']) for child in poll_result.findChildren()]
+#         #     #         num_players[nplayers]=[float(vote)/sum(votes) for vote in votes]
+#         #     #     print num_players
+#         #         #Num_players
+#         #         #Proposal: Weight "best" by 
+#         #         # Ages
+#         #         # if less than half of the people think that the maximimum age
+#         #         # is over 21, then take the mode age <21 as the maximum age
+#         #     #     boardgame_play_time=int(soup.playingtime['value'])
+#         #         # For website display only
+#                 bg['DESCRIPTION']=soup.description.text
+#                 bg['IMAGE']=soup.image.text
+#                 bg['YEAR']=soup.yearpublished['value']
+#                 insert_basics_cmd = 'INSERT INTO Basics VALUES('+",".join([bg['BOARDGAME_ID'],"'"+bg['BOARDGAME_NAME']+"'",bg['YEAR'],"'"+bg['IMAGE']+"'","'"+bg['DESCRIPTION']+"'"]) +');'
+#                 print insert_basics_cmd
 
+#         #     #     boardgame_mechanic=[ mechanic.attrs['value'] for mechanic in soup.find_all('link',type='boardgamemechanic')]
+#         #     #     boardgame_weight=float(soup.averageweight['value'])
+            
+#             # for comment in comments:
+#             #     insert_preferences_cmd = 'INSERT INTO Preferences VALUES('+','.join(["'"+comment.attrs['username']+"'",bg['BOARDGAME_ID'],"'"+bg['BOARDGAME_NAME']+"'",comment.attrs['rating']]) +');'
+#             #     # print insert_preferences_cmd
+#             #     cur.execute(insert_preferences_cmd)
+
+
+
+
+
+# # colNames = []
+# # formatStrings = []
+# # values = []
+
+# # for colName, value in bg.items():
+# #     if not value == None:
+# #         colNames.append(colName)
+# #         values.append(value)
+# #         formatStrings.append('%s')
+
+# # insertStatement = "INSERT INTO {0}({1}) VALUES({2})".format(
+# #     'Preferences', 
+# #     ', '.join(colNames), 
+# #     ', '.join(values))
+
+
+# # with con:
+#     # c.execute("INSERT INTO preferences (USER_NAME) VALUES ('GARY')")
+
+
+
+
+#     #     user_name=[comment.attrs['username'] for comment in comments]
+#     #     user_rating=[float(comment.attrs['rating']) for comment in comments]
+#     # else:
+#     #     [user_name.append(comment.attrs['username']) for comment in comments]
+#     #     [user_rating.append(float(comment.attrs['rating'])) for comment in comments]
+#     #     print len(user_rating)
+
+# # user=pd.Series(data=user_rating,index=user_name)
+# # count,division = np.histogram(user)
+
+# # print boardgame_name
+# # print boardgame_id
+# # print boardgame_family_name
+# # print boardgame_family_id
+# # print boardgame_min_player,boardgame_max_player
+# # print boardgame_play_time
+# # print boardgame_year
+# # print boardgame_image
+# # print boardgame_mechanic
+# # print boardgame_weight
+# # print users
 
